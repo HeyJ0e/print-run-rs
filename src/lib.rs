8d889b99d9bf74850d12be6bac87d3a68743d2f4
HeyJ0e <heyj0e@tuta.io>
2025-07-07 16:16:31 +0000

    feat: add duration option

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,7 @@ use syn::{
 #[derive(Debug, Default)]
 struct PrintRunArgs {
     colored: bool,
+    duration: bool,
     timestamps: bool,
 }
 
@@ -24,6 +25,7 @@ impl Parse for PrintRunArgs {
 
             match ident.to_string().as_str() {
                 "colored" => args.colored = true,
+                "duration" => args.duration = true,
                 "timestamps" => args.timestamps = true,
                 other => {
                     return Err(Error::new(
@@ -81,7 +83,31 @@ macro_rules! create_timestamp {
                 let seconds = total_secs % 60;
                 let ts = format!("{:02}:{:02}:{:02}.{:03}", hours, minutes, seconds, millis);
                 let ts = {#colorize}(ts);
-                format!("{} ", ts) // use colorless space as a separator
+                format!("{} ", ts) // don't colorize the separator
+            }
+        }
+    }};
+}
+
+macro_rules! create_duration {
+    ($colored:expr) => {{
+        let colorize = cond!($colored, colorize_fn!(Yellow), quote! { |txt: String| txt });
+        quote! {
+            |start: std::time::Instant| {
+                let elapsed = start.elapsed().as_nanos();
+                let dur =
+                    if elapsed < 1_000 {
+                        format!("{}ns", elapsed)
+                    } else if elapsed < 1_000_000 {
+                        format!("{:.2}µs", elapsed as f64 / 1_000.0)
+                    } else if elapsed < 1_000_000_000 {
+                        format!("{:.2}ms", elapsed as f64 / 1_000_000.0)
+                    } else {
+                        format!("{:.2}s", elapsed as f64 / 1_000_000_000.0)
+                    }
+                ;
+                let dur = {#colorize}(dur);
+                format!(" in {}", dur) // colorize the result only
             }
         }
     }};
@@ -97,33 +123,43 @@ pub fn print_run(attr: TokenStream, item: TokenStream) -> TokenStream {
         sig,
         block,
     } = input;
-    let fn_name = sig.ident.to_string();
     let PrintRunArgs {
         colored,
+        duration,
         timestamps,
     } = args;
+    let fn_name = sig.ident.to_string();
 
     // Create start/end function names
     let start = cond!(colored, colorize!(fn_name.clone(), Green), fn_name.clone());
     let end = cond!(colored, colorize!(fn_name.clone(), Cyan), fn_name.clone());
 
     // Create timestamp creator closure
-    let create_timestamp_fn = if timestamps {
-        create_timestamp!(colored)
-    } else {
+    let create_timestamp_fn = cond!(
+        timestamps,
+        create_timestamp!(colored),
         quote! { || "".to_string() }
-    };
+    );
+
+    // Create duration creator closures
+    let duration_fn = cond!(
+        duration,
+        create_duration!(colored),
+        quote! { |_| "".to_string() }
+    );
 
     // Wrap the original function body
     let new_block = quote! {
         {
             let ts = {#create_timestamp_fn}();
+            let start = std::time::Instant::now();
             println!("{}{} starting", ts, #start);
 
             let result = (|| #block)();
 
+            let dur = {#duration_fn}(start);
             let ts = {#create_timestamp_fn}();
-            println!("{}{} ended", ts, #end);
+            println!("{}{} ended{}", ts, #end, dur);
             result
         }
     };
